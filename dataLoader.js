0a3fae7af2ad6c04c24d35395e94bff45fe0db53
robideaux <99033093+robideaux@users.noreply.github.com>
2023-08-11 20:29:59 -0500

    Update dataLoader.js

--- dataLoader.js
+++ dataLoader.js
@@ -1,9 +1,5 @@
-// https://script.google.com/macros/s/AKfycbxIw-74yPSw2whrwVLfcZrMz-isu2a0jWArhpvBzShjsRmgPGjB4OqrGnPCFPbA4XKk/exec
-// https://script.google.com/macros/s/AKfycbyzd82_Ynp_xdyQTdjOK3a6Z-HJ1vkVpNTAXsoeHrg/dev
-var locationsUrl = "https://script.google.com/macros/s/AKfycbxIw-74yPSw2whrwVLfcZrMz-isu2a0jWArhpvBzShjsRmgPGjB4OqrGnPCFPbA4XKk/exec"
-
-function fetchData(callback) {
-    fetch(locationsUrl)
+function fetchData(path, callback) {
+    fetch(path)
         .then(res => res.json())
         .then(data => {
             console.log(data)
