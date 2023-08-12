f77070b8e243e956b7d97c00fc2f46cda5a37882
robideaux <99033093+robideaux@users.noreply.github.com>
2023-08-11 21:15:44 -0500

    Update dataLoader.js

--- dataLoader.js
+++ dataLoader.js
@@ -1,22 +1,5 @@
 var locationsUrl = "https://script.google.com/macros/s/AKfycbxIw-74yPSw2whrwVLfcZrMz-isu2a0jWArhpvBzShjsRmgPGjB4OqrGnPCFPbA4XKk/exec"
 
-function fetchData(magic, callback) {
-    var path = locationsUrl + "/" + magic
-    fetch(path)
-        .then(res => {
-            var d = res.json()
-            return res.json()
-        })
-        .then(data => {
-            console.log(data)
-            callback(data)
-        })
-        .catch(err => {
-            console.log("Error: " + err)
-            callback(null)
-        })
-}
-
 async function fetchDataAsync(magic, callback) {
     var path = locationsUrl + "?magic=" + magic
     var response = await fetch(path)
